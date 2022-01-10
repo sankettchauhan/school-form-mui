99d31b6ac889588957818d576162385184bc3d66
sankettchauhan <sanket.chauhan4@gmail.com>
2022-01-10 12:07:16 +0530

    add city component

--- src/components/City.js
+++ src/components/City.js
@@ -1,5 +1,56 @@
+import {
+  Box,
+  FormControl,
+  FormLabel,
+  Grid,
+  MenuItem,
+  Select,
+} from "@mui/material";
+import { City } from "country-state-city";
 import React from "react";
 
-export default function City() {
-  return <div></div>;
+export default function CityComponent({
+  stateCode,
+  countryCode,
+  value,
+  setValue,
+}) {
+  const CITIES = City.getCitiesOfState(countryCode, stateCode).map(
+    (city) => city.name
+  );
+  const handleChange = (event) => {
+    const cityName = event.target.value;
+    setValue("city", event.target.value);
+  };
+  return (
+    <Grid container>
+      <Grid
+        item
+        xs={6}
+        style={{
+          paddingRight: 20,
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "flex-end",
+        }}
+      >
+        <FormLabel component="legend" style={{ fontSize: "1em" }}>
+          City
+        </FormLabel>
+      </Grid>
+      <Grid item xs={6}>
+        <Box sx={{ minWidth: 120 }}>
+          <FormControl>
+            <Select value={value} onChange={handleChange} placeholder="City">
+              {CITIES.map((name) => (
+                <MenuItem key={name} value={name}>
+                  {name}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+        </Box>
+      </Grid>
+    </Grid>
+  );
 }
